203061caafbdf6320dd04f527080ccafe566ac80
Slimobedsama <ohwo.okoro@yahoo.com>
2022-11-01 04:06:26 +0100

    addition of the signup and login user controller

--- controller/userController.js
+++ controller/userController.js
@@ -38,3 +38,23 @@ exports.signup = async(req, res) => {
         res.status(400).json({msg: 'Bad request'});
     }
 }
+
+exports.login = async(req, res) => {
+    const {email, userName, password} = req.body;
+   try {
+    const foundEmail = await User.findOne({email});
+    const foundUsername = await User.findOne({userName})
+    const checkUser = foundEmail || foundUsername
+    if(checkUser) {
+       const checkPassword = await bcrypt.compare(password, checkUser.password);
+        if(checkPassword) {
+            const token = genToken(checkUser._id)
+            return res.status(200).json({msg: 'Login successfully', token, data: checkUser});
+        }
+        return res.status(400).json({msg: 'Invalid password'});
+    }
+    return res.status(400).json({msg: 'Invalid email or username'});
+   } catch (err) {
+    return res.status(400).json({msg: 'User does not exist'});
+   }
+}
