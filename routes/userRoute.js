8b9e9ca12eac3635edc6208170eb27fead208834
Slimobedsama <ohwo.okoro@yahoo.com>
2022-11-01 04:04:26 +0100

    modify the user route for all users with an authorization

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const Router = express.Router();
 const userController = require('../controller/userController');
+const auth = require('../middleware/authMiddleware');
 
-Router.get('/', userController.allUsers);
+Router.get('/', auth, userController.allUsers);
 Router.post('/signup', userController.signup);
+Router.post('/login', userController.login);
 
 
 module.exports = Router;
