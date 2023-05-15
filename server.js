4ac783a961bde2ac7fbeb13e1f68db352416bfb2
Slimobedsama <ohwo.okoro@yahoo.com>
2023-05-15 20:07:19 +0100

    added morgan middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const morgan = require('morgan');
 const blogRoute = require('./routes/blogRoute');
 const userRoute = require('./routes/userRoute');
 const db = require('./config/db');
@@ -8,9 +9,12 @@ const db = require('./config/db');
 const app = express();
 PORT = process.env.PORT;
 db()
-
+// PARSES JSON DATA
 app.use(express.json());
+// PARSES ENCONDED URL DATA
 app.use(express.urlencoded({extended: true}));
+//MIDDLEWARE FOR DATA LOGGER
+app.use(morgan('combined'));
 
 app.use('/api/blogs', blogRoute);
 app.use('/api/user', userRoute);
