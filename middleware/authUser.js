b3e9d9bac406105016ba5e6441e4cfcb9789aa7a
Slimobedsama <ohwo.okoro@yahoo.com>
2023-05-24 03:01:39 +0100

    modification of the verify token in the auth function of the authUser file

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
-const User = require('../model/userModel');
+// const User = require('../model/userModel');
 
-const auth = async(req, res, next) => {
+const auth = (req, res, next) => {
     let token = req.headers.authorization;
-    if(!token) {
-        return res.status(403).json({msg: 'A token is need for authentication'});
+    if(token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken)=> {
+            if(err) {
+                console.log(err.message)
+                res.status(401).json({error: 'Invalid Token'})
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        })
+    } else {
+        res.status(403).json({error: 'Forbidden'});
     }
-    try {
-        let verifiedUser = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(verifiedUser.id)
-        req.user = user
-    } catch (err) {
-        console.log(err)
-        return res.status(401).json({msg: 'Inavlid Token'});
-    }
-    return next();
 }
 
 module.exports = auth;
