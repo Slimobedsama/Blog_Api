cd5a674e2005118983a2f7ced76c08102209d998
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-01 02:03:39 +0100

    minimized the mongoose connection code

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = () => {
-    const database = process.env.DBURI;
-    mongoose.connect(database, {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.DBURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => console.log('Database connected'))
     .catch((err) => console.log(err))    
 }
