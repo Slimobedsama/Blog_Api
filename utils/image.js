456407cf2c196abe99af3046ba3078cdcfa96bb8
Slimobedsama <ohwo.okoro@yahoo.com>
2023-11-07 20:55:44 +0100

    addition of the file filter function for file type filtering

--- utils/image.js
+++ utils/image.js
@@ -10,11 +10,20 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = (req, file, cb)=> {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    } else {
+        cb(null, false);
+    }
+}
+
 const upload = multer({ 
     storage: storage,
     limits: {
         fileSize: 1024 * 1024 * 5 // MAXIMUM SIZE IS 5MB
-    } 
+    },
+    fileFilter: fileFilter
 })
 
 module.exports = upload;
