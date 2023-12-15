2f1f8831cc2dd32c9c8b47d4892214624b36996c
Slimobedsama <ohwo.okoro@yahoo.com>
2023-12-15 01:09:58 +0100

    modified the comment schema. userName changed to name and type from string to objectId

--- model/commentModel.js
+++ model/commentModel.js
@@ -2,15 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    userName: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    }
-}, {timestamps: true});
+    name: { type: Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, required: true }
+}, { timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema);
 module.exports = Comment;
