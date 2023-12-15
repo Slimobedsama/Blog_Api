3b527de791060ad9883311fe01890e47d7227bf0
Slimobedsama <ohwo.okoro@yahoo.com>
2023-12-15 01:40:50 +0100

    user validation for signup added to user route signup

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Router = express.Router();
 const userController = require('../controller/userController');
+const { userValidateSignup } = require('../utils/validation');
 
 Router.get('/', userController.allUsers);
 Router.get('/:id', userController.getSingleUser);
-Router.post('/signup', userController.signup);
+Router.post('/signup', userValidateSignup, userController.signup);
 Router.post('/login', userController.login);
 Router.patch('/:id', userController.updateUser);
 
