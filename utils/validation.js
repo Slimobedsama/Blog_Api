848b6d34d9e15ece3ab830bf0df30102020a4e6e
Slimobedsama <ohwo.okoro@yahoo.com>
2023-12-15 01:34:32 +0100

    user signup validation added to validation file

--- utils/validation.js
+++ utils/validation.js
@@ -30,6 +30,27 @@ const adminEditValidation =
     }
 ];
 
+// USER VALIDATION BELOW
+const userValidateSignup = 
+[
+    body('firstName').notEmpty().withMessage('Enter First Name'),
+    body('lastName').notEmpty().withMessage('Enter Last Name'),
+    body('userName').notEmpty().withMessage('Enter A username').custom( async value=> {
+        const checkUsername = await Admin.findOne({ userName: value });
+        if (checkUsername) throw new Error("This username is already in use");
+    }),
+    body('email').isEmail().withMessage('Enter A valid email address').custom( async value=> {
+        const checkEmail = await Admin.findOne({ email: value });
+        if (checkEmail) throw new Error("This email is already in use");
+    }),
+    body('password').isStrongPassword({ minLength: 6, minSymbols: 0 }).withMessage('Password must be a minimum of 6 characters and a combination of at least 1 of uppercase and lowercase'),
+    (req, res, next)=> {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array().map(err=> err.msg) });
+        return next();
+    }
+];
+
 // BLOG VALIDATIONS BELOW
 const blogValidation = 
 [
@@ -48,6 +69,7 @@ const blogValidation =
 
 module.exports = { 
     adminValidateSignup, 
-    adminEditValidation, 
+    adminEditValidation,
+    userValidateSignup,
     blogValidation 
 };
