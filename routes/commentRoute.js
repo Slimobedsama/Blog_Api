c336a5667f46fdbe12f263381052ffa454478acd
Slimobedsama <ohwo.okoro@yahoo.com>
2023-12-21 18:52:11 +0100

    authorization added to create and update comment routes

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Router = express.Router();
 const commentController = require('../controller/commentController');
+const { userAuth } = require('../middleware/auth');
 
 Router.get('/', commentController.every);
 Router.get('/:id', commentController.getOne);
-Router.post('/', commentController.create);
-Router.patch('/:id', commentController.edit);
+Router.post('/', userAuth, commentController.create);
+Router.patch('/:id', userAuth, commentController.edit);
 
 module.exports = Router;
